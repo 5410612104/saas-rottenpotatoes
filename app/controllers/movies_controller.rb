b6952f16489d852ca87275ce1c406e89859986cf
orendon <oarendon@gmail.com>
2012-06-17 14:17:31 -0500

    updated to mix filter & order

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,18 +8,20 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.get_all_ratings
+    @ordered_by = params[:order_by] if params.has_key? 'order_by'
 
     if params.has_key? 'ratings'
-      @checked_ratings = params[:ratings].keys
-      @movies = Movie.find_all_by_rating(@checked_ratings)
-    else  
+      @checked_ratings = params[:ratings]  
+      
+      if @ordered_by      
+        @movies = Movie.find_all_by_rating(@checked_ratings, :order => "#{@ordered_by} asc")
+      else
+        @movies = Movie.find_all_by_rating(@checked_ratings)
+      end
+    elsif @ordered_by
+      @movies = Movie.all(:order => "#{@ordered_by} asc")
+    else 
       @movies = Movie.all
-    end
-
-    if params.has_key? 'order_by'
-      @ordered_by = params[:order_by]
-      @movies.sort! { |x,y| x.rating <=> y.rating }
-      # @movies = Movie.all(:order => "#{@ordered_by} asc")
     end
   end
 
