2d6a4e77b0c085c4cd12a697b3f33a978e74101d
orendon <oarendon@gmail.com>
2012-06-17 13:41:29 -0500

    added two methods to validate checked-checkboxes and highlighted-columns

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,12 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def was_highlighted?(column)
+  	@ordered_by==column ? 'hilite' : ''
+  end
+
+  def was_checked?(rating)
+  	@checked_ratings.include? rating unless !@checked_ratings
+  end
 end
