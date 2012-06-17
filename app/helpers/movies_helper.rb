0384955b886ad37386379bf1f7b5aafc3aeb3e9c
orendon <oarendon@gmail.com>
2012-06-17 14:18:07 -0500

    added method to generate ordering params

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -11,4 +11,8 @@ module MoviesHelper
   def was_checked?(rating)
   	@checked_ratings.include? rating unless !@checked_ratings
   end
+
+  def create_order_params(column)
+  	{order_by: column, ratings: @checked_ratings}
+  end
 end
