e6c047b381289bfbb9109d2266e83d6c7af57b97
orendon <oarendon@gmail.com>
2012-06-25 10:27:41 -0500

    added update and find movies with same director

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -5,14 +5,16 @@ describe MoviesController do
 
 	before :each do
 		@fake_movie = stub('double').as_null_object
+		@fake_movie.stub(:director).and_return('fake director')
 		@movie = [mock('movie1')]
 	end
 
 	describe 'updating movie info' do
 		before :each do
-			Movie.should_receive(:find).with("1").and_return(@fake_movie)
+			movie_id = 5
+			Movie.should_receive(:find).with(movie_id.to_s).and_return(@fake_movie)
 			@fake_movie.should_receive(:update_attributes!).exactly 1
-			post :update, {:id => 1, :movie => @movie}
+			post :update, {:id => movie_id, :movie => @movie}
 		end
 		it 'should call the model method that performs the movie update' do
 			true
@@ -26,7 +28,39 @@ describe MoviesController do
 	end
 
 	describe 'finding movies by same director' do
-		pending "---- implement ----"
+		before :each do
+			@movie_id = 10
+			@founded = [mock('a movie'), mock('another one')]
+		end
+		it 'should render same_director view' do
+			Movie.stub(:find).and_return(@fake_movie)
+			Movie.stub(:find_all_by_director).and_return(@founded)
+
+			get :same_director, {:id => @movie_id}
+			response.should render_template 'same_director'
+		end
+		it 'should call Model method to get movies with same director' do
+			Movie.should_receive(:find).with(@movie_id.to_s).and_return(@fake_movie)
+			Movie.should_receive(:find_all_by_director).and_return(@founded)
+
+			get :same_director, {:id => @movie_id}
+		end
+		it 'should make founded movies available to the view' do
+			Movie.stub(:find).and_return(@fake_movie)
+			Movie.stub(:find_all_by_director).and_return(@founded)
+
+			get :same_director, {:id => @movie_id}
+
+			assigns(:movies).should == @founded
+		end
+		it 'should return to home page, if no movies founded' do
+			empty_director =  double('movie', :director => '').as_null_object
+			Movie.stub(:find).and_return(empty_director)
+			Movie.stub(:find_all_by_director)
+			
+			get :same_director, {:id => @movie_id}
+			response.should redirect_to(movies_path)						
+		end
 	end
 
 end
